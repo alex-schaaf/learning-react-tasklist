be4a862e6bbad2fa3952869d33d411290e04ec1f
Alex <17277800+alex-schaaf@users.noreply.github.com>
2021-02-17 09:49:00 +0100

    Tasks wroking

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Header from "./components/Header"
+import Tasks from "./components/Tasks"
+import { useState } from 'react'
 
-function App() {
+
+const App = () => {
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const [tasks, setTasks] = useState(
+    [
+      {
+        "id": 1,
+        "text": "Doctors Appointment",
+        "day": "Feb 5th at 2:30pm",
+        "reminder": true
+      },
+      {
+        "id": 2,
+        "text": "Meeting at School",
+        "day": "Feb 6th at 1:30pm",
+        "reminder": true
+      },
+      {
+        "id": 3,
+        "text": "Learn React",
+        "day": "Feb 7th at 2:34pm",
+        "reminder": false
+      }
+    ]
+  )
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />)
+        : ("No Tasks")}
     </div>
-  );
+  )
 }
+
+
 
 export default App;
