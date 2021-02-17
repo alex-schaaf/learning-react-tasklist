8a860652b397ae305703dbc12c961d00d67f553b
Alex <17277800+alex-schaaf@users.noreply.github.com>
2021-02-17 19:59:04 +0100

    add reminder toggle and add task form
    with toggle button

--- src/App.js
+++ src/App.js
@@ -3,11 +3,25 @@ import './App.css';
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
 import { useState } from 'react'
-
+import AddTask from "./components/AddTask"
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
+
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  function toggleReminder(id) {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
+  // Add Task
+  function addTask(task) {
+    const id = Math.floor(Math.random() * 100000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
   }
 
   const [tasks, setTasks] = useState(
@@ -36,8 +50,9 @@ const App = () => {
 
   return (
     <div className="container">
-      <Header />
-      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />)
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />)
         : ("No Tasks")}
     </div>
   )
